b8309d9c920b2eaef039ea57012d686de0c43922
Vrushparva Vaishnav <vrushparva27@gmail.com>
2018-02-08 13:41:13 +0530

    [ADD]

---
+++ learn python /ex8.py
@@ -0,0 +1,21 @@
+# tabby_cat = "\tI'm tabbed in"
+# persian_cat = "I'm split\non a line."
+# backslash_cat = "I'm \\ a \\ cat."
+#
+# fat_cat = """
+# I'll do a list:'
+# \t* cat food
+# \t* Fishies
+# \t* Catnip\n\t* Grass
+#
+# """
+#
+# print(tabby_cat)
+# print(persian_cat)
+# print(backslash_cat)
+# print(fat_cat)
+
+while True:
+    for i in [" ","b2b2b2b","b2    b2","b2    b2","b2b2b2b2","b2    b2","b2    b2","b2b2b2b"
+                "\n", "b2b2b2b", "      b2","      b2", "b2b2b2b2", "b2      ", "b2      ", "b2b2b2b2"]:
+        print("%s\r" % i,)
