c3a5946b7db5d569796bfdadaad6d26d45806691
Vrushparva Vaishnav <vrushparva27@gmail.com>
2018-02-13 20:37:56 +0530

    [ADDED New files..]

--- learnpython/ex11.py
+++ learnpython/ex11.py
@@ -2,14 +2,34 @@ from sys import argv
 
 script, filename = argv
 
-txt = open(filename)
-print("Here 's your file %r :" % filename)
-print(txt.read())
+print("We're going to erase %r." % filename)
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that,hit RETURN.")
 
-print("Type the filename again :")
-file_again = input("> ")
+input("?")
+
+print("Opening the File.....")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("Line 1:")
+line2 = input("Line 2:")
+line3 = input("Line 3:")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("/n")
+target.write(line2)
+target.write("/n")
+target.write(line3)
+target.write("/n")
 
 
-txt_again = open(file_again)
+print("And Finally , we close it.")
+target.close()
 
-print(txt_again.read())
