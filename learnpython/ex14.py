aedcecbdd6a16a09158fe5c7cff37cea35e57c4f
Vrushparva Vaishnav <vrushparva27@gmail.com>
2018-02-15 19:56:52 +0530

    [ADDED new file]

---
+++ learnpython/ex14.py
@@ -0,0 +1,24 @@
+def chees_and_crackers(cheese_count, boxes_of_crackers):
+    print("You have %d cheeses", cheese_count)
+    print("You have %d boxes of crackers", boxes_of_crackers)
+    print("Man that'senough for party")
+    print("Get a blanket\n")
+
+
+print(" we can just give function number directly:")
+chees_and_crackers(20,30)
+
+print("OR,we can use variavle for our scripts:")
+amount_of_cheese = 10
+amount_of_crackers = 20
+
+chees_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("WE can do match inside too:")
+chees_and_crackers(10+20, 5+6)
+
+
+print("And you can combine the two , variables amn math:")
+chees_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+
