87970c3003b271ca8fccad5ce1c4ca965251d79b
Vrushparva Vaishnav <vrushparva27@gmail.com>
2018-02-21 12:25:02 +0530

    [ADD]

---
+++ learnpython/ex17.py
@@ -0,0 +1,28 @@
+print("You enter a dark room with two doors. Do you go to door #1 or door #2?")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear here eating a cheese cake.")
+    print("1. Take the cake.")
+    print("2. Scream at the bear.")
+
+    bear = input("> ")
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your legs off. Good job!")
+    else:
+        print("Well, doing %s is probably better. Bear runs away.") % bear
+
+elif door == "2":
+    print("You stare into the endless abyss at Cthulhu's retina.")
+    print("1. Blueberries.")
+    print("Yellow jacket clothespins.")
+    print("Understanding revolvers yelling melodies.")
+
+    insanity = input("> ")
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello. Gof job.")
+    else:
+        print("The insanity rots your eyes into a pool of muck. Gof job.")
