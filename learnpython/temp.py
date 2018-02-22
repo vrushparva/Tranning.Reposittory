7b18f6cd430def2026736816be85351ca22687aa
Vrushparva Vaishnav <vrushparva27@gmail.com>
2018-02-22 12:21:38 +0530

    [Testing]

---
+++ learnpython/temp.py
@@ -0,0 +1,34 @@
+#
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+#
+# my_list = [n*n for n in num]
+# # print(my_list)
+#
+# for i in my_list:
+#     print(i)
+
+
+##############################################33
+# #
+# #
+# def square_numbers(nums):
+#     # results = []
+#     for n in nums:
+#         # results.append(n * n)
+#         yield (n*n)
+#
+# # nums = [1, 2, 3, 4, 5]
+# my_nums = square_numbers([1, 2, 3, 4, 5])
+# #
+# print(next(my_nums))
+# print(next(my_nums))
+# print(next(my_nums))
+# print(next(my_nums))
+# print(next(my_nums))
+
+# my_nums = (n*n for n in [1, 2, 3])
+# for i in my_nums:
+#     print(i)
+# print(list(my_nums))
+##############################################33
