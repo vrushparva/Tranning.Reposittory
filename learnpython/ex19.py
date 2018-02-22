41c88468907a86b9a88070bdcdb45d322e922e3a
Vrushparva Vaishnav <vrushparva27@gmail.com>
2018-02-22 13:05:38 +0530

    [MODIFIED]

--- learnpython/ex19.py
+++ learnpython/ex19.py
@@ -1,3 +1,5 @@
+# programme using while loop
+
 # i = 0
 # numbers = []
 #
@@ -12,21 +14,56 @@
 #         print(num)
 
 #############################################
-# upper programme using function
+# programme using function
 
+#
+# def loop():
+#     i = 0
+#     numbers = []
+#     while i < 6:
+#         print("The Top number is %d" % i)
+#         numbers.append(i)
+#         i += 1
+#         print("Now number is %r" % numbers)
+#         print("At the bottom i is %d " % i)
+#     print("The numbers:")
+#     for num in numbers:
+#         print(num)
+#
+#
+# print(loop())
 
-def loop():
-    i = 0
-    numbers = []
-    while i < 6:
-        print("The Top number is %d" % i)
-        numbers.append(i)
-        i += 1
-        print("Now number is %r" % numbers)
-        print("At the bottom i is %d " % i)
-    print("The numbers:")
-    for num in numbers:
-        print(num)
+#############################################
+# programme using for loop
+#
+# i = 0
+# numbers = []
+#
+# for i in range(0, 6):
+#     print("The Top number is %d" % i)
+#     numbers.append(i)
+#     i += 1
+#     print("Now number is %r" % numbers)
+#     print("At the bottom i is %d " % i)
+# print("The Numbers :")
+# for num in numbers:
+#         print(num)
 
+#############################################
 
-print(loop())
+# upper programme if condition
+
+# i = 0
+# numbers = []
+#
+# for i in range(6):
+#     if i < 6:
+#         print("The Top number is %d" % i)
+#         numbers.append(i)
+#         i += 1
+#         print("Now number is %r" % numbers)
+#         print("At the bottom i is %d " % i)
+#         print("The Numbers :")
+#
+# for num in numbers:
+#     print(num)
